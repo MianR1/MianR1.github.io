1e95400f2d48ba8d1053a4eb7314eca48fd82fac
Mian Rafay <93675507+MianR1@users.noreply.github.com>
2022-08-05 21:26:03 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+/*--live typing--*/
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+	navbarLinks.classList.toggle('active')
+});
+
+
+var typed = new Typed("#typing",{
+	strings:["","Web Designer","Web Developer","Student","Programmer","Gamer"],
+	typeSpeed:100,
+	backSpeed:60,
+	loop:true
+
+});
+
+
+var logoChange = new Typed("#typingLogo",{
+	strings:["Story","Passion","Future"],
+	typeSpeed:200,
+	backSpeed:200,
+	loop:true
+});
+
+
+	$(function () {
+	$(document).scroll(function(){
+var $nav = $("#mainNav");
+$nav.toggleClass("scrolled", $(this).scrollTop()>$nav.height());
+	});
+});
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+	anchor.addEventListener("click", function(e){
+		e.preventDefault();
+		document.querySelector(this.getAttribute("href")).scrollIntoView({
+			behavior : "smooth"
+		});
+	});
+});
+
+
